29cb071f62161b158f57547324a61a61af4b97cc
VictoriqueMoe <loli@victorique.moe>
2022-01-18 18:41:47 +0000

    added swagger

--- src/main/java/moe/victorique/OCR/OcrApplication.java
+++ src/main/java/moe/victorique/OCR/OcrApplication.java
@@ -1,9 +1,13 @@
 package moe.victorique.OCR;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = @Server(url = "https://api.victorique.moe"))
 public class OcrApplication {
 
 	public static void main(String[] args) {
